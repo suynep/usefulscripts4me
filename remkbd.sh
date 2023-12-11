08b245d24329e4cfcd2baf5ac7085e2dff92e39c
Suyash Nepal <105336284+suynep@users.noreply.github.com>
2023-12-11 22:23:04 +0545

    Update remkbd.sh

--- remkbd.sh
+++ remkbd.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+# This script `removes` the laptop's built-in keybd (helps me use moi MaxFit61 ez)
+
 ID=$(xinput list | grep AT | awk '{print $7}' | cut -d '=' -f 2)
 
 xinput float $ID
