915f9c3fb535983686d2930f32bf409de0806781
suy <suy@nepal.com>
2024-01-17 12:30:13 +0545

    modified p5create

--- p5create.sh
+++ p5create.sh
@@ -10,5 +10,17 @@ fi
 
 mkdir -p $1/assets/
 cp $DIRECTORY/p5.js $1/assets/ # note that $1 is a folder
-cd $1 && touch index.html script.js
+cd $1 && touch script.js && cp $DIRECTORY/index.html .
 
+workingdir=$(pwd)
+
+# Below SHIT is done just to match the `Title` of the Html to the current Project Name
+match='--Title--'
+insert=$1
+FILE='index.html'
+
+sed -i "s/$match/$insert/" $FILE
+
+ANOTHERFILE="$workingdir/script.js"
+
+echo -e "function setup() {\n\tcreateCanvas(400, 400);\n}\n\nfunction draw() {\n\tbackground(50);\n}" > $ANOTHERFILE
