09412298f1720f03717b629a2b2839a604e075c9
suywho <suy.nepal@gmail.com>
2024-10-04 21:04:49 +0545

    modify some, add polybarlaunch.sh

--- enable_natural_scrolling.sh
+++ enable_natural_scrolling.sh
@@ -4,6 +4,6 @@ id=$(xinput list | grep -v Elantech | grep Touchpad | awk '{print $6}' | cut -d 
 
 id1=$(xinput list-props $id | grep -v Default | grep 'Natural Scrolling' | awk '{print $5}' | cut -d '(' -f2 | cut -d ')' -f1)
 
-# xinput set-prop $id $id1 1
+xinput set-prop $id $id1 1
 
-echo -e "$id \n$id1"
+# echo -e "$id \n$id1"
